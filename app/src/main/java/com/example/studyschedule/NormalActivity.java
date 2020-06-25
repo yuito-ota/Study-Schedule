207ac6f8a35e95cc00a8257fc34abc5c61f6c811
yuito-ota <2020m039@kuas.ac.jp>
2020-06-26 04:42:27 +0900

    add scrollview

--- app/src/main/java/com/example/studyschedule/NormalActivity.java
+++ app/src/main/java/com/example/studyschedule/NormalActivity.java
@@ -1,8 +1,9 @@
 package com.example.studyschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NormalActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class NormalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+    }
+
+    public void normalToHome(View view) {Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
